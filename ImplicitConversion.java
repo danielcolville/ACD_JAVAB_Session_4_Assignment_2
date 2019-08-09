96684c214593df279b19dceb64aabf3d10b6f5e1
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-09 14:54:22 -0400

    Add files via upload

---
+++ ImplicitConversion.java
@@ -0,0 +1,12 @@
+package session4;
+
+public class ImplicitConversion {
+	public static void main(String[] args) {
+		double a;
+		int b=5;
+		double c=3.5;
+		System.out.println("Adding int :"+b+" and double " + c);
+		a=b+c;
+		System.out.println("Yields "+a+" which is a double");
+	}
+}
