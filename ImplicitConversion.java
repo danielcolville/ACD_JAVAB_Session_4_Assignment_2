b1f64a5b1d58f32b55286764d443245090b30a83
danielcolville <53836490+danielcolville@users.noreply.github.com>
2019-08-11 15:21:47 -0400

    Update ImplicitConversion.java

--- ImplicitConversion.java
+++ ImplicitConversion.java
@@ -5,8 +5,12 @@ public class ImplicitConversion {
 		double a;
 		int b=5;
 		double c=3.5;
-		System.out.println("Adding int :"+b+" and double " + c);
+		System.out.println("Adding int "+b+" and double " + c);
 		a=b+c;
 		System.out.println("Yields "+a+" which is a double");
+		
+		System.out.println("Dividing b by c also gives a double:");
+		double d=b/c;
+		System.out.println(d);
 	}
 }
